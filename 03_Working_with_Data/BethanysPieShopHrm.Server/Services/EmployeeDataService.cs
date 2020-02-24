589a1902762837b5521acfd80be6db0764262b45
Yves Schelpe <yves.schelpe@kdg.be>
2020-02-24 14:15:46 +0100

    JsonSerializerOptions now ok

--- 03_Working_with_Data/BethanysPieShopHrm.Server/Services/EmployeeDataService.cs
+++ 03_Working_with_Data/BethanysPieShopHrm.Server/Services/EmployeeDataService.cs
@@ -16,12 +16,17 @@ namespace BethanysPieShopHrm.Server.Services
             => _httpClient = httpClient;
 
         public async Task<IEnumerable<Employee>> GetEmployees()
-            => await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
-                (await _httpClient.GetStreamAsync($"api/employee"), new JsonSerializerOptions());
+        {
+
+            var employeesJsonData = await _httpClient.GetStreamAsync($"api/employee");
+            var employees = await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>(employeesJsonData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return employees;
+        }
 
         public async Task<Employee> GetEmployeeDetails(int employeeId)
             => await JsonSerializer.DeserializeAsync<Employee>
-                (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions());
+                (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
         public Task<Employee> AddEmployee(Employee employee) 
             => throw new NotImplementedException();
